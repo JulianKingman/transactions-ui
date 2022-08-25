927fe52bfac96b706170b988e527b02f6fbc05ab
JulianKingman <julian.kingman@gmail.com>
2022-08-24 19:39:52 -0500

    setup code styling + transaction type

---
+++ jest.config.ts
@@ -0,0 +1,12 @@
+import type { Config } from "@jest/types";
+
+// Sync object
+const config: Config.InitialOptions = {
+  preset: "jest-expo",
+  transformIgnorePatterns: [
+    "node_modules/(?!((jest-)?react-native|@react-native(-community)?)|expo(nent)?|@expo(nent)?/.*|@expo-google-fonts/.*|react-navigation|@react-navigation/.*|@unimodules/.*|unimodules|sentry-expo|native-base|react-native-svg)",
+  ],
+  verbose: true,
+};
+
+export default config;
