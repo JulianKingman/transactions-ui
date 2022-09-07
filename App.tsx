e43862b42669ae824889f63144ecfcfd862d7a7e
JulianKingman <julian.kingman@gmail.com>
2022-09-06 18:37:51 -0700

    cleanup

--- App.tsx
+++ App.tsx
@@ -30,12 +30,3 @@ export default function App() {
     </SafeAreaView>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#223222',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
